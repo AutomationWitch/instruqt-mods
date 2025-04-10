15273fc91aeaa43fd77f0ef761fc380f0bdb6caa
AutomationWitch <53153683+AutomationWitch@users.noreply.github.com>
2025-04-10 17:59:29 +0200

    Update satellite_mod.sh

--- satellite/satellite_mod.sh
+++ satellite/satellite_mod.sh
@@ -50,4 +50,6 @@ ssh -o Stricthostkeychecking=no rhel2 subscription-manager register --org Acme_O
 hammer scap-content bulk-upload --type default
 hammer policy create --organization Demo --deploy-by ansible --name "Hardening Baseline" --scap-content-id 1 --scap-content-profile-id 9 --period weekly --weekday saturday
 
-hammer hostgroup create --name Red --ansible-role-ids 1,5,62  --openscap-proxy-id 1
+hammer hostgroup create --name Red --ansible-role-ids 1,5,62 --openscap-proxy-id 1
+hammer host update --hostgroup Red --name rhel1
+hammer host update --hostgroup Red --name rhel2
