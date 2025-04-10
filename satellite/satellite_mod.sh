cd1f79f4c9b2f784ce2a2ec90cdd44129ad94811
AutomationWitch <53153683+AutomationWitch@users.noreply.github.com>
2025-04-10 16:15:28 +0200

    Update satellite_mod.sh

--- satellite/satellite_mod.sh
+++ satellite/satellite_mod.sh
@@ -1,7 +1,8 @@
-!#/bin/bash
+# Cosmetic changes
 hammer organization update --id 1 --name Demo
 hammer location update --id 2 --name Paris
 
+# Content 1/2
 hammer content-view create --organization Demo --name OS --repository-ids 4
 hammer content-view create --organization Demo --name Apps --repository-ids 3
 
@@ -15,13 +16,12 @@ hammer lifecycle-environment create --organization Demo --name Prod --prior Dev
 hammer content-view publish --organization Demo --name OS --lifecycle-environments Dev,Prod
 hammer content-view publish --organization Demo --name Apps --lifecycle-environments Dev,Prod
 
-
 hammer content-view publish --organization Demo --lifecycle-environments Dev,Prod --name OS
 hammer content-view publish --organization Demo --lifecycle-environments Dev,Prod --name Apps
 
-
-ssh-copy-id -i ~foreman-proxy/.ssh/id_rsa_foreman_proxy.pub root@rhel1
-ssh-copy-id -i ~foreman-proxy/.ssh/id_rsa_foreman_proxy.pub root@rhel2
+# Hosts configuration
+ssh-copy-id -o Stricthostkeychecking=no -i ~foreman-proxy/.ssh/id_rsa_foreman_proxy.pub root@rhel1
+ssh-copy-id -o Stricthostkeychecking=no -i ~foreman-proxy/.ssh/id_rsa_foreman_proxy.pub root@rhel2
 
 ssh -o Stricthostkeychecking=no rhel1 wget https://satellite.lab/pub/katello-ca-consumer-latest.noarch.rpm --no-check-certificate
 ssh -o Stricthostkeychecking=no rhel1 dnf install katello-ca-consumer-latest.noarch.rpm -y
@@ -29,13 +29,15 @@ ssh -o Stricthostkeychecking=no rhel1 dnf install katello-ca-consumer-latest.noa
 ssh -o Stricthostkeychecking=no rhel2 wget https://satellite.lab/pub/katello-ca-consumer-latest.noarch.rpm --no-check-certificate
 ssh -o Stricthostkeychecking=no rhel2 dnf install katello-ca-consumer-latest.noarch.rpm -y
 
+# Compliance
 hammer scap-content bulk-upload --type default
 
-# We're doing this one later because of lock issue
+# Content 2/2
 hammer content-view publish --organization Demo --lifecycle-environments Dev,Prod --name RHEL9
 
 hammer activation-key create --organization Demo --content-view RHEL9 --lifecycle-environment Dev --name RHEL9_Dev
 hammer activation-key create --organization Demo --content-view RHEL9 --lifecycle-environment Prod --name RHEL9_Prod
 
+# Hosts registration
 ssh -o Stricthostkeychecking=no rhel1 subscription-manager register --org Acme_Org --activationkey RHEL9_Dev
 ssh -o Stricthostkeychecking=no rhel2 subscription-manager register --org Acme_Org --activationkey RHEL9_Prod
