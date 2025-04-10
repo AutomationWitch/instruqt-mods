29d39719f08475536f7c4a656abaa22a98ae45c5
AutomationWitch <53153683+AutomationWitch@users.noreply.github.com>
2025-04-10 17:21:16 +0200

    Update satellite_mod.sh

--- satellite/satellite_mod.sh
+++ satellite/satellite_mod.sh
@@ -3,21 +3,26 @@ hammer organization update --id 1 --name Demo
 hammer location update --id 2 --name $CITY
 
 # Content 1/2
-hammer content-view create --organization Demo --name OS --repository-ids 4
-hammer content-view create --organization Demo --name Apps --repository-ids 3,8
-
-hammer content-view create --organization Demo --name RHEL9 --composite --auto-publish true
-hammer content-view component add --organization Demo --composite-content-view RHEL9 --latest --component-content-view OS
-hammer content-view component add --organization Demo --composite-content-view RHEL9 --latest --component-content-view Apps
-
 hammer lifecycle-environment create --organization Demo --name Dev --prior Library
 hammer lifecycle-environment create --organization Demo --name Prod --prior Dev
 
+hammer content-view create --organization Demo --name RHEL9 --composite --auto-publish true
+
+hammer content-view create --organization Demo --name OS --repository-ids 4
+hammer content-view create --organization Demo --name Apps --repository-ids 3
+hammer content-view create --organization Demo --name Tools --repository-ids 8
+
+hammer content-view component add --organization Demo --composite-content-view RHEL9 --latest --component-content-view OS
+hammer content-view component add --organization Demo --composite-content-view RHEL9 --latest --component-content-view Apps
+hammer content-view component add --organization Demo --composite-content-view RHEL9 --latest --component-content-view Tools
+
 hammer content-view publish --organization Demo --name OS --lifecycle-environments Dev,Prod
 hammer content-view publish --organization Demo --name Apps --lifecycle-environments Dev,Prod
+hammer content-view publish --organization Demo --name Tools --lifecycle-environments Dev,Prod
 
 hammer content-view publish --organization Demo --lifecycle-environments Dev,Prod --name OS
 hammer content-view publish --organization Demo --lifecycle-environments Dev,Prod --name Apps
+hammer content-view publish --organization Demo --lifecycle-environments Dev,Prod --name Tools
 
 # Hosts configuration
 ssh-copy-id -o Stricthostkeychecking=no -i ~foreman-proxy/.ssh/id_rsa_foreman_proxy.pub root@rhel1
